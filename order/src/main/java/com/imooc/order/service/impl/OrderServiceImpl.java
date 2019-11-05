50c4011b70e35fe8f4f162669700ab238bf141fe
jiangchengyu <24728440050@qq.com>
2019-11-05 17:17:23 +0800

    完成下单流程。

--- order/src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
@@ -1,6 +1,10 @@
 package com.imooc.order.service.impl;
 
+import com.imooc.order.client.ProductClient;
+import com.imooc.order.dataobject.OrderDetail;
 import com.imooc.order.dataobject.OrderMaster;
+import com.imooc.order.dataobject.ProductInfo;
+import com.imooc.order.dto.CartDTO;
 import com.imooc.order.dto.OrderDTO;
 import com.imooc.order.enums.OrderStatusEnum;
 import com.imooc.order.enums.PayStatusEnum;
@@ -13,6 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 /**
  * @ClassName OrderServiceImpl
@@ -29,21 +37,48 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderDetailRepository orderDetailRepository;
 
+    @Autowired
+    private ProductClient productClient;
+
     /**
      * @param orderDTO 订单数据
      * @return 创建的订单实体
      */
     @Override
     public OrderDTO create(OrderDTO orderDTO) {
+        String orderId = KeyUtil.genUniqueKey();
 
-        // todo 2. 查询商品信息（调用商品服务）
-        // todo 3. 计算总价
-        // todo 4. 扣库存（调用商品服务）
-        // 5. 订单入库
+        //  查询商品信息（调用商品服务）
+        List<String> productIdList = orderDTO.getOrderDetailList().stream().map(OrderDetail::getProductId).collect(Collectors.toList());
+        List<ProductInfo> productInfoList = productClient.listForOrder(productIdList);
+        // 计算总价
+        AtomicReference<BigDecimal> orderAmount = new AtomicReference<>(new BigDecimal(BigInteger.ZERO));
+        orderDTO.getOrderDetailList().forEach(orderDetail -> {
+            productInfoList.forEach(productInfo -> {
+                if (orderDetail.getProductId().equals(productInfo.getProductId())) {
+                    // 单价*数量
+                    orderAmount.set(productInfo.getProductPrice()
+                            .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                            .add(orderAmount.get()));
+
+                    BeanUtils.copyProperties(productInfo, orderDetail);
+                    orderDetail.setOrderId(orderId);
+                    orderDetail.setDetailId(KeyUtil.genUniqueKey());
+                    // 订单详情入库
+                    orderDetailRepository.save(orderDetail);
+                }
+            });
+        });
+        // 扣库存（调用商品服务）
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream()
+                .map(e -> new CartDTO(e.getProductId(), e.getProductQuantity())).collect(Collectors.toList());
+        productClient.decreaseStock(cartDTOList);
+
+        // 订单入库
         OrderMaster orderMaster = new OrderMaster();
-        orderDTO.setOrderId(KeyUtil.genUniqueKey());
+        orderDTO.setOrderId(orderId);
         BeanUtils.copyProperties(orderDTO, orderMaster);
-        orderMaster.setOrderAmount(new BigDecimal(5));
+        orderMaster.setOrderAmount(orderAmount.get());
         orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
         orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
 
