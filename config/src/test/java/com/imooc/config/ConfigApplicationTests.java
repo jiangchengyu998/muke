4abbb3247fa3bfe3f436046f7f68e3f2c9d3d555
jiangchengyu <24728440050@qq.com>
2019-11-06 16:24:39 +0800

    完成config 服务

---
+++ config/src/test/java/com/imooc/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
