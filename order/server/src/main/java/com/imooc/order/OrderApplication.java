2bf0f60ce4a97bd645514675f52399e4068edc97
jiangchengyu <jchengyu0829@163.com>
2019-11-29 11:21:16 +0800

    引入hystrix超时设置。

--- order/server/src/main/java/com/imooc/order/OrderApplication.java
+++ order/server/src/main/java/com/imooc/order/OrderApplication.java
@@ -2,11 +2,15 @@ package com.imooc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient    // 作为
+//@SpringBootApplication
+//@EnableDiscoveryClient    // 作为
+//@EnableCircuitBreaker
+@SpringCloudApplication // 包含上面的三個
 @EnableFeignClients(basePackages = "com.imooc.product.client")
 public class OrderApplication {
 
--- product/server/src/main/java/com/imooc/product/controller/ProductController.java
+++ product/server/src/main/java/com/imooc/product/controller/ProductController.java
@@ -82,6 +82,11 @@ public class ProductController {
      */
     @PostMapping("/listForOrder")
     public List<ProductInfoOutput> listForOrder(@RequestBody List<String> productList) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return productService.findList(productList);
     }
 
