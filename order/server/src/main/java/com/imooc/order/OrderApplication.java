ec307e336f379aa6c66c25636611a26c35d04957
jiangchengyu <jchengyu0829@163.com>
2019-11-29 12:20:13 +0800

    feign 使用hystrix

--- order/server/src/main/java/com/imooc/order/OrderApplication.java
+++ order/server/src/main/java/com/imooc/order/OrderApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication
 //@EnableDiscoveryClient    // 作为
 //@EnableCircuitBreaker
 @SpringCloudApplication // 包含上面的三個
 @EnableFeignClients(basePackages = "com.imooc.product.client")
+@ComponentScan(basePackages = "com.imooc")
 public class OrderApplication {
 
     public static void main(String[] args) {
--- product/client/src/main/java/com/imooc/product/client/ProductClient.java
+++ product/client/src/main/java/com/imooc/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.imooc.product.client;
 import com.imooc.product.common.DecreaseStockInput;
 import com.imooc.product.common.ProductInfoOutput;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ import java.util.List;
  * @Date 2019/11/6
  * @Version V1.0
  **/
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @GetMapping("/msg")
@@ -27,4 +28,23 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component
+    static class ProductClientFallback implements ProductClient{
+
+        @Override
+        public String productMsg() {
+            return null;
+        }
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
