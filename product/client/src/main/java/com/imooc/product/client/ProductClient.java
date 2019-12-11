c07f2a39082b96b9cc2645fff3bd58c1ee8d9bac
jiangchengyu <jchengyu0829@163.com>
2019-12-11 22:49:17 +0800

    完成product升級

--- product/client/src/main/java/com/imooc/product/client/ProductClient.java
+++ product/client/src/main/java/com/imooc/product/client/ProductClient.java
@@ -2,7 +2,8 @@ package com.imooc.product.client;
 
 import com.imooc.product.common.DecreaseStockInput;
 import com.imooc.product.common.ProductInfoOutput;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
