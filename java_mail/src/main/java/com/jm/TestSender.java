b91fb95a39a4a4806b0889056d554bde2983fddf
jiangchengyu <jchengyu0829@163.com>
2020-09-09 11:17:59 +0800

    完成邮件的发送

---
+++ java_mail/src/main/java/com/jm/TestSender.java
@@ -0,0 +1,7 @@
+package com.jm;
+
+public class TestSender {
+    public static void main(String[] args) throws Exception {
+        SendEmailByWY.sender("291550492@qq.com", "收益提醒","你个机车仔，你很亮啊！");
+    }
+}
