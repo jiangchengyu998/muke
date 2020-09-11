d503a6d51a48fa539477baa7cbc7322037662444
jiangchengyu <jchengyu0829@163.com>
2020-09-11 15:35:59 +0800

    引入Google guava EventBus

--- patterns/src/main/java/com/p/v_3/MetricsCollector.java
+++ patterns/src/main/java/com/p/v_3/MetricsCollector.java
@@ -1,18 +1,33 @@
 package com.p.v_3;
 
 
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.concurrent.Executors;
+
 public class MetricsCollector {
+
+    private static final int DEFAULT_STORAGE_THREAD_POOL_SIZE = 20;
+
     private MetricsStorage metricsStorage;//基于接口而非实现编程
+    private EventBus eventBus;
 
     public MetricsCollector() {
-        this(new RedisMetricsStorage());
+        this(new RedisMetricsStorage(), DEFAULT_STORAGE_THREAD_POOL_SIZE);
+    }
+
+    public MetricsCollector(MetricsStorage metricsStorage, int threadNumToSaveData) {
+        this.metricsStorage = metricsStorage;
+        this.eventBus = new AsyncEventBus(Executors.newFixedThreadPool(threadNumToSaveData));
+        this.eventBus.register(new EventListener());
     }
 
     //依赖注入
     public MetricsCollector(MetricsStorage metricsStorage) {
-        this.metricsStorage = metricsStorage;
+        this(metricsStorage, DEFAULT_STORAGE_THREAD_POOL_SIZE);
     }
 
     //用一个函数代替了最小原型中的两个函数
@@ -20,7 +35,14 @@ public class MetricsCollector {
         if (requestInfo == null || StringUtils.isBlank(requestInfo.getApiName())) {
             return;
         }
-        metricsStorage.saveRequestInfo(requestInfo);
+        eventBus.post(requestInfo);
+    }
+
+    public class EventListener {
+        @Subscribe
+        public void saveRequestInfo(RequestInfo requestInfo) {
+            metricsStorage.saveRequestInfo(requestInfo);
+        }
     }
 }
 
