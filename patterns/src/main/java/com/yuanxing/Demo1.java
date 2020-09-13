d1f9033f675edc6ccdd447f9751bc11c26896b8a
jiangchengyu <jchengyu0829@163.com>
2020-09-13 21:55:36 +0800

    原型模式

---
+++ patterns/src/main/java/com/yuanxing/Demo1.java
@@ -0,0 +1,28 @@
+package com.yuanxing;
+
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class Demo1 {
+    private HashMap<String, SearchWord> currentKeywords=new HashMap<>();
+
+    public void refresh() {
+        HashMap<String, SearchWord> newKeywords = new LinkedHashMap<>();
+
+        // 从数据库中取出所有的数据，放入到newKeywords中
+        List<SearchWord> toBeUpdatedSearchWords = getSearchWords();
+        assert toBeUpdatedSearchWords != null;
+        for (SearchWord searchWord : toBeUpdatedSearchWords) {
+            newKeywords.put(searchWord.getKeyword(), searchWord);
+        }
+
+        currentKeywords = newKeywords;
+    }
+
+    private List<SearchWord> getSearchWords() {
+        // TODO: 从数据库中取出所有的数据
+        return null;
+    }
+}
