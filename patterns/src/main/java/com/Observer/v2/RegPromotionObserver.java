59db4352583281ecdd6fd0f86f20904c5898738c
jiangchengyu <jchengyu0829@163.com>
2020-09-22 14:12:56 +0800

    实现异步的方式

--- patterns/src/main/java/com/Observer/v2/RegPromotionObserver.java
+++ patterns/src/main/java/com/Observer/v2/RegPromotionObserver.java
@@ -3,8 +3,13 @@ package com.Observer.v2;
 public class RegPromotionObserver implements RegObserver {
     private PromotionService promotionService; // 依赖注入
 
+    /**
+     * 使用异步的方式
+     * @param userId
+     */
     @Override
     public void handleRegSuccess(long userId) {
-        promotionService.issueNewUserExperienceCash(userId);
+        Thread thread = new Thread(() -> promotionService.issueNewUserExperienceCash(userId));
+        thread.start();
     }
 }
--- patterns/src/main/java/com/Observer/v2/UserController.java
+++ patterns/src/main/java/com/Observer/v2/UserController.java
@@ -19,10 +19,14 @@ public class UserController {
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 public class UserController {
     private UserService userService; // 依赖注入
     private List<RegObserver> regObservers = new ArrayList<>();
+    private Executor executor;
+
+    public UserController(Executor executor) {    this.executor = executor;  }
 
     // 一次性设置好，之后也不可能动态的修改
     public void setRegObservers(List<RegObserver> observers) {
@@ -35,7 +39,12 @@ public class UserController {
         long userId = userService.register(telephone, password);
 
         for (RegObserver observer : regObservers) {
-            observer.handleRegSuccess(userId);
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    observer.handleRegSuccess(userId);
+                }
+            });
         }
 
         return userId;
