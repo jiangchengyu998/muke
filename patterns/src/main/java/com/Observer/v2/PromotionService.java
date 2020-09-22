3e31cc0e41d5f3c34d7b02aceab448c04a064aaf
jiangchengyu <jchengyu0829@163.com>
2020-09-22 14:07:17 +0800

    用户注册的离职

---
+++ patterns/src/main/java/com/Observer/v2/PromotionService.java
@@ -0,0 +1,7 @@
+package com.Observer.v2;
+
+public class PromotionService {
+    public void issueNewUserExperienceCash(long userId) {
+
+    }
+}
---
+++ patterns/src/main/java/com/Observer/v2/RegObserver.java
@@ -0,0 +1,7 @@
+package com.Observer.v2;
+
+
+public interface RegObserver {
+    void handleRegSuccess(long userId);
+}
+
