d918ac51f79124b5f334923720184880efe81c53
jiangchengyu <jchengyu0829@163.com>
2020-10-28 07:41:53 +0800

    springboot

---
+++ springboot-helloworld/src/main/java/com/java0829/demo/bean/Dog.java
@@ -0,0 +1,35 @@
+package com.java0829.demo.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "person.dog")
+public class Dog {
+    private String name;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
